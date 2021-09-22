a06abefc672963e0fc82c5f6b1ff9baf79929d74
XZPshaw <zepeng@ualberta.ca>
2021-09-21 19:53:15 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 #  coding: utf-8 
 import socketserver
-
+import os
+import urllib.parse
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -27,12 +28,112 @@ import socketserver
 # try: curl -v -X GET http://127.0.0.1:8080/
 
 
+
+#https://emalsha.wordpress.com/2016/11/24/how-create-http-server-using-python-socket-part-ii/
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+
+        self.root = os.path.abspath(".")
+        self.host = "http://127.0.0.1:8000"
+        self.str_data = self.data.decode('utf-8')
+        print("decoded request:\n",self.str_data)
+        a = self.str_data.split('\n')[0].split()
+        print("decoded:",a)
+
+        method = self.str_data.splitlines()[0].split()[0]
+        relativePath = self.str_data.splitlines()[0].split()[1]
+        #httpVersion = self.data.spliteline()[0].decode('utf-8').split()[-1]
+
+        base_dir = "www"
+        file_path = os.path.abspath(base_dir) + relativePath
+        print(file_path)
+        if method == 'GET' :
+            content_type = ''
+            print("relativePath",relativePath.split('/'))
+            if not relativePath.endswith("/") and '.' not in relativePath.split('/')[-1]:
+                response = self.status301Res(file_path)
+
+                #print("redirect",file_path +"/")
+
+            elif relativePath[-1] == '/' and ".css" not in relativePath and ".html" not in relativePath:
+                file_path += 'index.html'
+                
+                try :
+                    print("final path:",file_path)
+                    target_file = open(file_path,'r')
+                    content_file = target_file.read()
+                except FileNotFoundError:
+                    response = self.status404Res()
+                else:
+                    target_file.close()
+                    content_type = 'text/html'
+                    response = self.status200Res(content_type,content_file)
+            else:
+                try:
+                    print("path:",file_path)
+                    target_file = open(file_path,'r')
+                    content_file = target_file.read()
+                except FileNotFoundError:
+                    response = self.status404Res()
+                else:
+                    target_file.close()
+                    if (".css" in relativePath.split('/')[-1]):
+                        print("css here")
+                        content_type = 'text/css'
+                    elif (".html" in relativePath.split('/')[-1]):
+                        print("html here")
+                        content_type = 'text/html'
+
+                    response = self.status200Res(content_type,content_file)
+        else:
+            response = self.status405Res()
+
+        #https://stackoverflow.com/questions/10114224/how-to-properly-send-http-response-with-python-using-socket-library-only/10114266
+        print(response)
+        #self.request.sendall(response)
+        self.request.sendall(bytearray(response,'utf-8'))
+
+    def status301Res(self,file_path):
+        
+        res = "HTTP/1.1 301 Moved Permanently\r\n"
+        res += "Content-Type: text/html\r\n"
+        #res += "Content-Length: 0\r\n"
+        res += "Connection: close\r\n"
+        targetLocation = file_path+"/"
+        res += "Location: %s"%targetLocation
+        
+
+        return res
+        
+
+        #self.request.sendall(response)
+
+
+    def status200Res(self,content_type,content_file):
+        '''
+        res = "HTTP/1.1 200 OK\r\n"
+        res += "Connection: close\r\n"
+        if len(content_type)>0:
+            res += "Content-Type:%s"%content_type
+        res += content_file
+        res +="\r\n"
+        '''
+        res = "HTTP/1.1 200 OK\r\n" + "Content-Type:%s\r\n"%content_type + content_file + "\r\n"
+        #self.request.sendall(content_file)
+        return res
+    def status404Res(self):
+        res = "HTTP/1.1 404 Not Found\r\n"
+        #res += "Content-Type: text/html\r\n"
+        res += "Connection: close\r\n"
+        return res
+    def status405Res(self):
+        res = "HTTP/1.1 405 Method Not Allowed\r\n"
+        res += "Connection: close\r\n"
+        return res
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
